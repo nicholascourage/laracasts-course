c3fb0e582031b6adb96b93d1598b0a57e1b65a69
Nick Courage <nicholascourage@gmail.com>
2025-04-19 23:17:37 +0100

    S2.01 - Using Full Page Components

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Livewire\Search;
+use App\Livewire\ShowArticle;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
+
+Route::get('/search', Search::class);
+Route::get('/articles/{article}', ShowArticle::class);
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
