c99a870c3fb15cd335734de8fda267617e8b94e6
Nick Courage <nicholascourage@gmail.com>
2025-04-21 20:54:57 +0100

    S2.04 - Navigating with Navigate

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -30,10 +30,8 @@
                 </div>
             </div>
         </div>
-        <script>
-            document.addEventListener('search:clear-results', function(e){
-                console.log('test');
-            });
+        <script data-navigate-once>
+                console.log('page loaded');
         </script>
     </body>
 </html>
