0e19342771d175c64fc930318f1f03cb897c40c4
Nick Courage <nicholascourage@gmail.com>
2025-04-22 10:49:10 +0100

    S2.05 - Iterating Over Collections

---
+++ app/Livewire/ArticleIndex.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Article;
+use Livewire\Component;
+
+class ArticleIndex extends Component
+{
+
+    public function render()
+    {
+        return view('livewire.article-index', [
+            'articles' => Article::all()
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Livewire\ArticleIndex;
 use App\Livewire\Search;
 use App\Livewire\ShowArticle;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', ArticleIndex::class);
 
 Route::get('/search', Search::class);
 Route::get('/articles/{article}', ShowArticle::class);
